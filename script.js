dc595af46abb51d114f7dfdb43869140406fcd49
Ollie Khakwani <okhakwani@gmail.com>
2017-06-30 09:12:40 -0700

    added basics of api request, but getting access control error

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ $(document).ready(function() {
   console.log("working!");
 
   if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(getWeatherData);
   }
 
-  function showPosition(location) {
-    console.log(location.coords.latitude);
+  function getWeatherData(position) {
+    console.log(position.coords.latitude);
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    $('.location').html("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
+
+    //Make request for weather and print
+    //https://api.darksky.net/forecast/ca6136574e3da0e0a583474d13934794/
+    $.getJSON("https://api.darksky.net/forecast/ca6136574e3da0e0a583474d13934794/" + lat + "," + lon, function(response) {
+      console.log(response);
+    });
   }
 });
