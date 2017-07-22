2f438c4e7eaedae36d19e1321c118a9580b18880
Ollie Khakwani <okhakwani@gmail.com>
2017-07-22 09:08:16 -0400

    now showing timestamp and min max temp in grid

--- script.js
+++ script.js
@@ -11,10 +11,33 @@ $(document).ready(function() {
     var lon = position.coords.longitude;
     $('.location').html("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
 
-    //Make request for weather and print
+    //Make request for weather using lat and lon params and print
     //https://api.darksky.net/forecast/ca6136574e3da0e0a583474d13934794/
-    $.getJSON("https://api.darksky.net/forecast/ca6136574e3da0e0a583474d13934794/" + lat + "," + lon, function(response) {
+    $.getJSON("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/ca6136574e3da0e0a583474d13934794/" + lat + "," + lon, function(response) {
       console.log(response);
+      showForecast(response);
     });
   }
 });
+
+function showForecast(data) {
+  var fullForecast = data.daily.data;
+  fullForecast.forEach(function(dayForecast) {
+    showDayForecast(dayForecast);
+  });
+};
+
+function showDayForecast(data) {
+  // console.log("day forecast function");
+  var temperatureString = Math.round(data.apparentTemperatureMin) + "F / " + Math.round(data.apparentTemperatureMax) + "F";
+
+  var newElementHtml = "<div class='row'><div class='col-sm-6'>";
+  newElementHtml += data.time;
+  newElementHtml += "</div><div class='col-sm-6'>";
+  newElementHtml += temperatureString;
+  newElementHtml += "</div>";
+
+  newElementHtml += "</div>"
+
+  $('#forecast-display').append(newElementHtml);
+}
