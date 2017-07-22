18fa6939d79bc585d7cd96f58430cdb31d3e0d6f
Ollie Khakwani <okhakwani@gmail.com>
2017-07-22 10:34:23 -0400

    added skycons

--- script.js
+++ script.js
@@ -22,36 +22,43 @@ $(document).ready(function() {
 
 function showForecast(data) {
   var fullForecast = data.daily.data;
-  fullForecast.forEach(function(dayForecast) {
-    showDayForecast(dayForecast);
+  fullForecast.forEach(function(dayForecast, index) {
+    showDayForecast(dayForecast, index);
   });
 };
 
-function showDayForecast(data) {
-  // console.log("day forecast function");
+function showDayForecast(data, index) {
+  var skycons = new Skycons({"color": "black"});
+  // skycons.add("icon1", Skycons.RAIN);
+
+  // Create date output strings
   var day = moment.unix(data.time);
   var dayOfWeek = "<p class='mb-0'>" + day.format("dddd") + "</p>";
   var dateString = "<p class='mb-0'>" + day.format("MMM Do") + "</p>";
 
+  // Create weather detail output strings
   var temperatureString = "<p class='mb-0'>" + Math.round(data.apparentTemperatureMin) + "F / " + Math.round(data.apparentTemperatureMax) + "F" + "</p>";
-
   var descriptionString = "<p class='mb-0'>" + data.summary + "</p>";
-
   var rainString = "<p class='mb-0'>" + data.precipProbability*100 + "% chance of rain" + "</p>";
 
+  // Create day forecast container
   var newElementHtml = "<div class='row day-forecast'>";
 
   // Add date column and info
-  newElementHtml += "<div class='col-sm-6'>" + dayOfWeek + dateString + "</div>";
+  newElementHtml += "<div class='col-sm-3'>" + dayOfWeek + dateString + "</div>";
+
+  // Add container for Skycons. Add the skycon after HTML is appended
+  var canvasName = "icon_" + index;
+  newElementHtml += "<div class='col-sm-3'>" + "<canvas id='" + canvasName + "' width='128' height='128'></canvas>" + "</div>";
 
   // Add weather details
-  newElementHtml += "<div class='col-sm-6'>";
-  newElementHtml += temperatureString;
-  newElementHtml += descriptionString;
-  newElementHtml += rainString;
-  newElementHtml += "</div>";
+  newElementHtml += "<div class='col-sm-6'>" + temperatureString + descriptionString + rainString + "</div>";
 
   newElementHtml += "</div>"
 
   $('#forecast-display').append(newElementHtml);
+
+  // Add appropriate Skycon (after the canvas has actually been appended)
+  skycons.add(canvasName, data.icon);
+  skycons.play();
 }
